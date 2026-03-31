d5519a446ed68caf4cbc90cd4077a2285c5f26c3
lingshin <yokaringx@outlook.com>
2026-03-31 20:28:50 +0800

    Nix Flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Client/server to synchronize media playback on mpv/VLC/MPC-HC/MPC-BE on many computers";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.callPackage ./default.nix {};
+      };
+    };
+}
